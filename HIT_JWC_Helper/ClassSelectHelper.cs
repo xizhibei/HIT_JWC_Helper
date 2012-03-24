903103beff365dff29784526e0ed57b43c22fb62
Xu Zhipei <xuzhipei@gmail.com>
2012-03-24 18:56:54 +0800

    Little modify on Readme, and add executable program just for convenient

---
+++ HIT_JWC_Helper/ClassSelectHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HIT_JWC_Helper
+{
+    /// <summary>
+    /// Not finish yet.
+    /// </summary>
+    class ClassSelectHelper
+    {
+        public void fetchClassInfo(string input)
+        {
+            MatchCollection general = Regex.Matches(input,
+                "<div[^>]+>[^<]+</div>",
+                RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            MatchCollection time_and_location = Regex.Matches(input,
+                "<div[^>]+>[^<]+<br>[^>]+</div>",
+                RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            MatchCollection other = Regex.Matches(input,
+                "<div[^>]+>[^<]+<br>[^>]+</div>",
+                RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+        }
+    }
+}
